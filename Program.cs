9027b0f1451737c71ee747abda4d81c2128f8d2d
Irseora <irseora@gmail.com>
2022-12-03 23:51:15 +0200

    Done

--- Program.cs
+++ Program.cs
@@ -19,9 +19,16 @@ namespace ASC_Hex_Viewer
 {
     class Program
     {
+        // Numarul de caractere pe care le afiseaza programul pe o linie
+        static int numberOfCharsToRead = 16;
+
         static void Main(string[] args)
         {
+            // Citeste numele unui fisier
+            Console.Clear();
+            Console.WriteLine("Hex Viewer: Programul va afisa continutul unui fisier in format hexazecimal.");
             string path = ReadPath();
+            Console.WriteLine("-------------------------------------------------------------------------------");
 
             try
             {
@@ -34,6 +41,10 @@ namespace ASC_Hex_Viewer
                 Console.WriteLine(e.Message);
             }
 
+            // Programul se incheie dupa ce se apasa o tasta
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine("Apasati orice tasta pentru a incheia programul...");
+            Console.ReadKey();
         }
 
         /// <summary> Citeste numele unui fisier </summary>
@@ -47,7 +58,7 @@ namespace ASC_Hex_Viewer
             while (!File.Exists(path))
             {
                 Console.WriteLine("Fisierul introdus nu exista!");
-                Console.WriteLine("Introduceti numele fisierului dorit: ");
+                Console.Write("Introduceti numele fisierului dorit: ");
 
                 path = Console.ReadLine();
             }
@@ -55,28 +66,90 @@ namespace ASC_Hex_Viewer
             return path;
         }
 
-        /// <summary> Afiseaza continutul unui fisier text in format hexazecimal </summary>
+        /// <summary> Afiseaza continutul unui fisier in format hexazecimal </summary>
         /// <param name="path"> Numele fisierului care va fi citit </param>
         static void HexViewer(string path)
         {
             // Creeaza un StreamReader pentru a citi din fisier, se inchide la finalul directivei using
             using (var reader = new StreamReader(path))
             {
-                // Citeste cate 16 octeti pana la finalul fisierului
-                int charactersRead = 0;
-                char[] buffer = {};
-                do
+                // Citeste cate numberOfCharsToRead octeti pana la finalul fisierului
+                int charactersReadTotal = 0, charactersReadCurrent = -1;
+                char[] buffer = new char[numberOfCharsToRead];
+
+                while (charactersReadCurrent != 0)
                 {
-                    reader.Read(buffer, 0, 16);
-                    charactersRead += buffer.Length;
+                    charactersReadCurrent = reader.Read(buffer, 0, numberOfCharsToRead);
 
-                    Console.WriteLine("{0:X}", charactersRead)
+                    // Indexul la care se afla primul octet din bufferul curent
+                    Console.Write("{0:X8}: ", charactersReadTotal);
 
-                } while (buffer != null);
+                    // Afiseaza sirul de caractere citit sub forma unor cifre hexazecimale
+                    Afis("hexa", buffer, charactersReadCurrent);
 
-                
+                    // Afiseaza sirul de caractere citit sub forma de text
+                    Afis("text", buffer, charactersReadCurrent);                    
 
+                    Console.WriteLine();
+                    charactersReadTotal += charactersReadCurrent;
+                }
+            }
+        }
 
+        /// <summary> Afiseaza continutul unui sir de caractere in formatul dat </summary>
+        /// <param name="format"> Formatul in care se va afisa sirul de caractere </param>
+        /// <param name="buffer"> Sirul de caractere care va fi afisat </param>
+        /// <param name="charactersReadCurrent"> Numarul de caractere care trebuie afisate </param>
+        static void Afis(string format, char[] buffer, int charactersReadCurrent)
+        {
+            // Afiseaza continutul sirului de caractere
+            int i;
+            for (i = 0; i < charactersReadCurrent; i++)
+            {
+                // In format hexazecimal
+                if (format == "hexa")
+                {
+                    Console.Write("{0:X2} ", (int)buffer[i]);
+
+                    // Imparte in 2 jumatati sirul de cifre hexazecimale
+                    if (i == 7)
+                        Console.Write("| ");
+                }
+                // In format text
+                else if (format == "text")
+                {
+                    // Afiseaza caracterul '|' pentru a delimita cifrele hexazecimale de text
+                    if (i == 0)
+                        Console.Write(" | ");
+
+                    // Daca caracterul se poate afisa, il afiseaza (litera, cifra, semn de punctuatie, spatiu)
+                    if (char.IsLetterOrDigit(buffer[i]) || char.IsPunctuation(buffer[i]) || buffer[i] == ' ')
+                        Console.Write(buffer[i]);
+                    // Daca caracterul nu se poate afisa, afiseaza caracterul '.'
+                    else
+                        Console.Write('.');
+                }
+            }
+
+            // Daca nu s-au citit numberOfCharsToRead caractere
+            for (int j = i; j < numberOfCharsToRead; j++)
+            {
+                // Completeaza cu 0-uri coloana cifrelor hexazecimale
+                if (format == "hexa")
+                {
+                    Console.Write("00 ");
+
+                    // Imparte in 2 jumatati sirul de cifre hexazecimale
+                    if (j == 7) Console.Write("| ");
+                }
+                // Completeaza cu puncte coloana textului
+                else if (format == "text")
+                {
+                    // Afiseaza caracterul '|' pentru a delimita cifrele hexazecimale de text
+                    if (j == 0) Console.Write(" | ");
+
+                    Console.Write('.');
+                }
             }
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ASC_Hex_Viewer
  
+The Cascade Tunnel refers to two railroad tunnels (original and its replacement) in the northwest United States, east of the Seattle metropolitan area in the Cascade Range of Washington, at Stevens Pass.
+It is approximately 65 miles (105 km) east of Everett, with both portals adjacent to U.S. Route 2.
+Both single-track tunnels were constructed by the Great Northern Railway.
+
+The first was 2.63 miles (4.23 km) in length and opened in 1900 to avoid problems caused by heavy winter snowfalls on the original line that had eight zig zags (switchbacks).
+The current tunnel is 7.8 miles (12.6 km) in length and entered service in early 1929,[1][2] approximately 1.5 miles (2.4 km) south of and 500 feet (150 m) lower in elevation than the original.
+
+The present east portal is nearly four miles (6.5 km) east of the original's and is at 2,881 feet (878 m) above sea level, 1,180 feet (360 m) below the pass.
+The tunnel connects Berne in Chelan County on its east with Scenic Hot Springs in King County on its west and is the longest railroad tunnel in the United States.
